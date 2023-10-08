f55919d09ed7a0051ccaf1bdfa66197324f1228c
Nathan <106090674+SirNatan1@users.noreply.github.com>
2023-10-08 13:50:01 +0300

    Update ECS-Deployment.sh
    updated logic for index determination

--- ECS-Deployment.sh
+++ ECS-Deployment.sh
@@ -1,13 +1,15 @@
 # Fetch the task definition using AWS CLI
 task_definition=`(aws ecs describe-task-definition --task-definition "$task_def" --no-cli-pager)`
-# Set the index based on a condition
-if [ "$container_name" = "crm" ]; then
-  CONTAINER_INDEX=0
+# Determine the index based on the container name in the task definition
+CONTAINER_INDEX=`(echo -E "$task_definition" | jq --arg CONTAINER "$container_name" '.taskDefinition.containerDefinitions | map(.name) | index($CONTAINER)')`
+# Check if the container name was found in the task definition
+if [ "$CONTAINER_INDEX" != "null" ]; then
+  # Update the image in the task definition for the selected container index
+  NEW_TASK_DEFINITION=`(echo -E "$task_definition" | jq --arg IMAGE "$(ecr_name):$(Build.SourceVersion)" --argjson INDEX "$CONTAINER_INDEX" '.taskDefinition | .containerDefinitions[$INDEX].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')`
 else
-  CONTAINER_INDEX=1
+  echo "Container name '$container_name' not found in the task definition."
+  exit 1
 fi
-# Update the image in the task definition for the selected container index
-NEW_TASK_DEFINITION=$(echo -E $task_definition | jq --arg IMAGE "$ecr_name:$image_id" --argjson INDEX $CONTAINER_INDEX '.taskDefinition | .containerDefinitions[$INDEX].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
 # Print the updated task definition
 echo $NEW_TASK_DEFINITION
 # Remove unnecessary fields from the JSON
